0847833a01e665c4c21aa5ac6627abd471b784df
mayanktiwari101 <iammayanktheprogrammer@gmail.com>
2026-08-11 17:32:35 +0530

    transitionDone

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+
+    $('.navbar-button').click(function () {
+        $('.navbar-button').toggleClass("change");
+    });
+
+    // $(window).scroll(function () {
+    //     let position = $(this).scrollTop();
+    //     if (position >= 200) {
+    //         $('.nav-menu').addClass('custom-navbar');
+    //     } else {
+    //         $('.nav-menu').removeClass('custom-navbar');
+    //     }
+    // }); 
+
+
+    
+});
