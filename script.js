5008b1540b03bfdfa8332d5f1a9d6996f2100766
mayanktiwari101 <iammayanktheprogrammer@gmail.com>
2026-08-13 00:21:52 +0530

    galleryDid

--- script.js
+++ script.js
@@ -12,5 +12,20 @@ $(document).ready(function () {
     //         $('.nav-menu').removeClass('custom-navbar');
     //     }
     // }); 
-    
+
+    $('.gallery-list-item').click(function() {
+        let value = $(this).attr('data-filter');
+        if (value === 'all') {
+            $('.filter').show(300);
+        }
+        else {
+            $('.filter').not('.' + value).hide(300);
+            $('.filter').filter('.' + value).show(300);
+        }
+    });
+
+    $('.gallery-list-item').click(function () {
+        $(this).addClass('active-item').siblings().removeClass('active-item');
+    });
+
 });
